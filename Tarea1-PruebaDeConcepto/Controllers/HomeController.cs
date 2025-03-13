3c2aac2534ad070b02a336677069e17943be3728
Helena0504 <helenamaria.vargasq@gmail.com>
2025-03-12 18:59:25 -0600

    ArregloUrl

---
+++ Tarea1-PruebaDeConcepto/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tarea1_PruebaDeConcepto.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("API is running!");
+        }
+    }
+}
+
---
+++ Tarea1-PruebaDeConcepto/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapFallbackToFile("VistaUsuario.html");
+
+app.Run();
+
+
